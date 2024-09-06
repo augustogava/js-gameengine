b50e21548631f4249ee090e1d8aa7b54ef11417d
Augusto Gava <augustodj@gmail.com>
2024-09-06 00:26:32 -0300

    Perlin (#17)
    Perlin

---
+++ js/ObjectMain.js
@@ -0,0 +1,106 @@
+class ObjectMain {
+    constructor(instance, mass, position, degrees, speed, direction) {
+        this.id = Utils.randomIntFromInterval(1, 5000);
+
+        this.color = "rgba(250,128,114, 1)";
+        this.colorProjection = Utils.changeRedSoftness(this.color, 200);
+        this.colorShadow = Utils.changeRedSoftness(this.color, 0);
+
+        this.G = 667.4;
+        this.mass = mass;
+        this.angle = (degrees)  ? ( degrees * (Math.PI / 180) ) : 0;
+        this.angularVelocity = 0;
+        this.position = position;
+        this.positionOld = position;
+        this.acceleration = 0;
+
+        this.positionFixed = position;
+        this.positionOldFixed = position;
+        this.accelerationFixed = 0;
+
+        this.acceleration = new Vector(0, 0);
+        
+        this.velocity = new Vector(0, 0);
+        if( speed )
+		    this.velocity.setLength(speed);
+
+        if( direction )
+		    this.velocity.setAngle(direction);
+
+        this.velocityFixed = this.velocity;
+
+
+        this.statePhysics = true;
+
+        if( mass ){
+            this.physics = new Physics(mass);
+            this.physics.addForce(this.physics.gravity.multiplyBy(this.mass));
+        }
+
+        this.SHADOW_HISTORY = 20;
+        this.historyShadow = [];
+        this.currentTime = Date.now();
+
+        // this.debugObj = new Debug(this);
+    }
+
+    debug(){            
+                  
+    }
+
+    collidesWith(otherShape) {
+        return false;
+    }
+
+    changePhysicsState(v) {
+        if (v == undefined)
+            this.statePhysics = true;
+
+        this.statePhysics = !this.statePhysics;;
+    }
+
+    updateDegrees(d) {
+        this.angle = (d)  ? ( d * (Math.PI / 180) ) : this.angle;
+    }
+
+    updatePosition(v) {
+        this.position = v;
+    }
+
+    commonsUpdate(obj){
+        obj.updateToReadbleNumber();
+        if (Globals.isAttraction()) {
+            obj.attract();
+        }
+
+        if (!Globals.isDebug()) {
+            return ;
+        }
+
+        // var timeDifference = Date.now() - obj.currentTime ;
+        // if ( (timeDifference > ( .01 * 1000)) ) {
+        //     obj.historyShadow.push({ x: obj.position.x, y: obj.position.y, angle: obj.angle, width: obj.width, height: obj.height });
+        //     obj.currentTime = Date.now();
+        // }
+        
+        // if (obj.historyShadow.length > this.SHADOW_HISTORY) {
+        //     obj.historyShadow.shift();
+        // }
+    }
+
+    updateToReadbleNumber(){
+        if( this.position )
+            this.positionFixed = new Vector( this.position.x.toFixed(1), this.position.y.toFixed(1) );      
+
+        if( this.positionOld )
+            this.positionOldFixed = new Vector( this.positionOld.x.toFixed(1), this.positionOld.y.toFixed(1) );      
+
+        if( this.velocity )
+            this.velocityFixed = new Vector( this.velocity.x.toFixed(1), this.velocity.y.toFixed(1) ); 
+    }
+
+    clone() {
+        // return new Vector(this.x, this.y);
+    }
+
+}
